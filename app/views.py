4f8d8521f00c72f1a83265ac29188ab35b73080e
samwel-chege <samwelchegeh09@gmail.com>
2021-08-02 15:01:00 +0300

    add relevant changes

--- app/views.py
+++ app/views.py
@@ -9,8 +9,8 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    message = 'Hello World'
-    return render_template('index.html',message  = message)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html',title  = title)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
@@ -18,4 +18,5 @@ def movie(movie_id):
     '''
     View movie page function that returns the movie details page and its data
     '''
-    return render_template('movie.html',id = movie_id)    
+    title = f'You are viewing {movie_id} '
+    return render_template('movie.html',title = title)    
