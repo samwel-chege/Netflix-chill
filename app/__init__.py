c8554ceaac7eb2912d769528727141ebf5373533
samwel-chege <samwelchegeh09@gmail.com>
2021-08-04 09:38:53 +0300

    initialize the bootsrap class

--- app/__init__.py
+++ app/__init__.py
@@ -9,4 +9,7 @@ app = Flask(__name__,instance_relative_config= True)
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
+# Initializing Flask Extensions 
+bootstrap  = Bootstrap (app)
+
 from app import views
