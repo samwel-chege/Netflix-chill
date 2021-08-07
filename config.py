5ddc152381abde45fd05cac9689714baba65da5f
samwel-chege <samwelchegeh09@gmail.com>
2021-08-07 12:41:18 +0300

    remove whitespace

--- config.py
+++ config.py
@@ -7,8 +7,6 @@ class Config:
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
-
-
 class ProdConfig(Config):
     '''
     Production  configuration child class
