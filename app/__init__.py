4da7d0e5841d89e1c79731c6a4b924f41629f65a
samwel-chege <samwelchegeh09@gmail.com>
2021-08-11 01:19:30 +0300

    initialize SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 
 def create_app(config_name):
