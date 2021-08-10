e8dc6b232a6add6e0fdfd7a09fc49047772a0671
samwel-chege <samwelchegeh09@gmail.com>
2021-08-11 01:25:16 +0300

    call the init_app method and pass in the app instance

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,7 @@ def create_app(config_name):
 
  #Initializing flask extensions
  bootstrap.init_app(app)
+ db.init_app(app)
 
  #Registering the blueprint
  from .main import main as main_blueprint
