b3bc46f003064b5377c02ad729f4f85c8c628701
samwel-chege <samwelchegeh09@gmail.com>
2021-08-12 23:44:33 +0300

    create a blueprint instance

---
+++ app/auth/__init__.py
@@ -0,0 +1,5 @@
+from flask import render_template
+from flask.blueprints import Blueprint
+auth = Blueprint('auth',__name__)
+
+from . import views
---
+++ app/auth/views.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from . import auth
+
+@auth.route('/login')
+def login():
+    return render_template('auth/login.html')
+
+    '''
+    A views function that renders a template file
+    '''
