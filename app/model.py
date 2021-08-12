497c8636056393054aaad91c9082836831bf47c8
samwel-chege <samwelchegeh09@gmail.com>
2021-08-13 00:34:38 +0300

    add an email column in the user class

--- app/model.py
+++ app/model.py
@@ -1,14 +1,16 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255),index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    pass_secure = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
 
     @property
     def password (self):
