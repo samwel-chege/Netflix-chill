e61c8fe636ad12a0b391eae1c9b66abf01d03fa0
samwel-chege <samwelchegeh09@gmail.com>
2021-08-12 23:43:17 +0300

    register the blueprint instance in the create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,8 @@ bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 
+
+
 def create_app(config_name):
 
 
@@ -24,6 +26,9 @@ def create_app(config_name):
  from .main import main as main_blueprint
  app.register_blueprint(main_blueprint)
 
+ from .auth import auth as auth_blueprint
+ app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
  #setting config
  from .request import configure_request
  configure_request(app)
@@ -31,20 +36,3 @@ def create_app(config_name):
 
  return app
 
-#  # Registering the blueprint
-#  from .main import main as main_blueprint
-#  app.register_blueprint(main_blueprint) 
-
-#  # Setting up configuration
-#  app.config.from_object(DevConfig)
-#  app.config.from_pyfile("config.py")
-
-#  # Initializing Flask Extensions 
-#  bootstrap  = Bootstrap (app)
-
-
-
-
-
-# from app import views
-# from app import error
