5e761877d9c6187e6523b1b1f53d10a0094624d7
samwel-chege <samwelchegeh09@gmail.com>
2021-08-12 22:37:50 +0300

    create verify_password method

--- app/model.py
+++ app/model.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 class User(db.Model):
@@ -7,6 +8,19 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    pass_secure = db.Column(db.String(255))
+
+    @property
+    def password (self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self,password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
 
     def __repr__(self):
         return f'User {self.username} '
