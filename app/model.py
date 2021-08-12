f705fd77f0d2784a592c6cfa332ab56e26b9bf17
samwel-chege <samwelchegeh09@gmail.com>
2021-08-12 09:20:49 +0300

    create a shell context

--- app/model.py
+++ app/model.py
@@ -1,3 +1,11 @@
+from . import db
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username} '
 class movie:
     '''
     Movie class to define Movie objects
--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
+from app.model import User
+
 
 #creating app instance
 app = create_app('development')
@@ -13,6 +15,10 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db, User = User)    
 
 if __name__ == '__main__':
     manager.run()
