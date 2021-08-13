234fecb38477741d03868154e2786d98d61f7109
samwel-chege <samwelchegeh09@gmail.com>
2021-08-13 11:22:39 +0300

    create a callback function

--- app/model.py
+++ app/model.py
@@ -3,9 +3,9 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.query.get(int(user_id))
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class User(UserMixin,db.Model):
