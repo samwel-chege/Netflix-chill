dabbfbbe8d8c517c98c3d6877d64bc3f7cf093e1
samwel-chege <samwelchegeh09@gmail.com>
2021-08-13 17:36:25 +0300

    restructure code

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,27 +4,8 @@ from . import auth
 from ..model import User
 from .. import db
 from .forms import LoginForm,RegistrationForm
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 
-
-@auth.route('/login')
-def login():
-    return render_template('auth/login.html')
-
-    '''
-    A views function that renders a template file
-    '''
-@auth.route('/register',methods = ["GET","POST"]) 
-def register():
-    form  = RegistrationForm()  
-    if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data) 
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('auth.login'))
-        title = "New Account"
-
-    return render_template('auth/register.html',registration_form = form)    
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -39,3 +20,26 @@ def login():
 
         title = "watchlist login"
         return render_template('auth/login.html',login_form = login_form,title = title)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
+
+
+@auth.route('/register',methods = ["GET","POST"]) 
+def register():
+    form  = RegistrationForm()  
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data) 
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+
+    return render_template('auth/register.html',registration_form = form)    
+
+
+
