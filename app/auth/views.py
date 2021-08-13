9ec03f77e1fd472bc37b15f9579b86735217a543
samwel-chege <samwelchegeh09@gmail.com>
2021-08-13 15:44:22 +0300

    create a form instance and pass it in the template

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,9 @@
-from flask import render_template
+from app.auth.forms import RegistrationForm
+from flask import render_template,redirect,url_for
 from . import auth
+from ..model import User
+from .. import db
+
 
 @auth.route('/login')
 def login():
@@ -8,3 +12,14 @@ def login():
     '''
     A views function that renders a template file
     '''
+@auth.route('/register',methods = ["GET","POST"]) 
+def register():
+    form  = RegistrationForm()  
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data) 
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+
+    return render_template('auth/register.html',registration_form = form)    
