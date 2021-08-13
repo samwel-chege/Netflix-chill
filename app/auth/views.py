4c8a3f14db2f754d2967474561f18ef287f71a31
samwel-chege <samwelchegeh09@gmail.com>
2021-08-13 16:59:34 +0300

    create an instance of the loginForm

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,10 @@
 from app.auth.forms import RegistrationForm
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,flash,request
 from . import auth
 from ..model import User
 from .. import db
+from .forms import LoginForm,RegistrationForm
+from flask_login import login_user
 
 
 @auth.route('/login')
@@ -23,3 +25,17 @@ def register():
         title = "New Account"
 
     return render_template('auth/register.html',registration_form = form)    
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or password')    
+
+        title = "watchlist login"
+        return render_template('auth/login.html',login_form = login_form,title = title)
