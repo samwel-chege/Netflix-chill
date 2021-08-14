4378cba918cc8058097893a6df5f52a165ad7663
samwel-chege <samwelchegeh09@gmail.com>
2021-08-15 00:21:14 +0300

    set up the SMTP server

--- config.py
+++ config.py
@@ -11,6 +11,13 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sammie:samm@localhost/watchlist'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
+    #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
     @staticmethod
     def init_app(app):
         pass
