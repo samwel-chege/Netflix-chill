0e05cdc8f4e9c6fcc40871d2e6b20f365438472f
samwel-chege <samwelchegeh09@gmail.com>
2021-08-15 01:08:13 +0300

    call the function inside the register view function

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,6 +5,7 @@ from ..model import User
 from .. import db
 from .forms import LoginForm,RegistrationForm
 from flask_login import login_user,logout_user,login_required
+from ..email import mail_message
 
 
 @auth.route('/login',methods=['GET','POST'])
@@ -36,6 +37,9 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data) 
         db.session.add(user)
         db.session.commit()
+
+        mail_message("Welcome to watchlist","email/welcome_user",user.email,user = user)
+
         return redirect(url_for('auth.login'))
         title = "New Account"
 
