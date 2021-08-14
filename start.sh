c4b4f8210cf772aa14e210ead878a1cb88f4908b
samwel-chege <samwelchegeh09@gmail.com>
2021-08-15 00:47:52 +0300

    create a mail_message function

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 export MOVIE_API_KEY='30ca3733f7df7e33ed7fd4376d4be540'
 export SECRET_KEY='5460cd45453a4vf53354ag45h5'
 
-export MAIL_USERNAME=samwelchegeh09@gmail.com
-export MAIL_PASSWORD = sammie7
+export MAIL_USERNAME='samwel.chege@student.moringaschool.com'
+export MAIL_PASSWORD = 'sammie7'
 
 
 python3 manage.py server
