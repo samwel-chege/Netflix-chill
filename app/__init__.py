92527df64f04586d4987938448e0a13103da4729
samwel-chege <samwelchegeh09@gmail.com>
2021-08-14 21:44:37 +0300

    create a configure_uploads function

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
 
 
 def create_app(config_name):
@@ -20,7 +22,9 @@ def create_app(config_name):
  #creating the app configurations
  app.config.from_object(config_options[config_name]) 
  
-
+  #Configure UploadSet
+ configure_uploads(app,photos)
+  
  #Initializing flask extensions
  bootstrap.init_app(app)
  login_manager.init_app(app)
