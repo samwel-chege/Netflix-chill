1ecd90ab3ec999dc19bff5728e06477169e155da
samwel-chege <samwelchegeh09@gmail.com>
2021-08-15 00:08:39 +0300

    import photos instance from the application factory module

--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,7 @@ from ..request import get_movies,get_movie,search_movie
 from .forms import ReviewForm,UpdateProfile
 from ..model import Review,User
 from flask_login import login_required
-from .. import db
+from .. import db,photos
 
 
 
@@ -93,4 +93,15 @@ def update_profile(uname):
 
         return redirect(url_for('.profile',uname = user.username))
 
-    return render_template('profile/update.html',form = form)    
+    return render_template('profile/update.html',form = form)  
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))     
