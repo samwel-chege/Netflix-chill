fefc699244c2eb6a707c5afea6d682c1b63a643d
samwel-chege <samwelchegeh09@gmail.com>
2021-08-15 10:38:47 +0300

    create two methods

--- app/model.py
+++ app/model.py
@@ -2,6 +2,7 @@ from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
+from datetime import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -18,6 +19,7 @@ class User(UserMixin,db.Model):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     password_secure = db.Column(db.String(255))
+    reviews = db.Column(db.Integer,db.ForeignKey("users.id"))
 
     @property
     def password (self):
@@ -43,6 +45,26 @@ class Role(db.Model):
 
     def ___repr__(self):
         return f'User {self.name}'   
+
+class Review(db.model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer,primary_key = True)
+    movie_id = db.Column(db.Integer)
+    movie_title = db.Column(db.String)
+    image_path = db.Column(db.String)
+    movie_review = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id")) 
+
+    def save_review(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_reviews(cls,id):
+        reviews = Review.query.filter_by(movie_id = id).all()
+        return reviews           
 
 class movie:
     '''
