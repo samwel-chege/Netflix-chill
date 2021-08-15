26f8e0cf0a2eb8b419cf511acd88a3f95fac3f7d
samwel-chege <samwelchegeh09@gmail.com>
2021-08-15 18:03:40 +0300

    create a new view function

--- app/main/views.py
+++ app/main/views.py
@@ -5,6 +5,7 @@ from .forms import ReviewForm,UpdateProfile
 from ..model import Review,User
 from flask_login import login_required,current_user
 from .. import db,photos
+import markdown2
 
 
 
@@ -109,3 +110,11 @@ def update_pic(uname):
         user.profile_pic_path = path
         db.session.commit()
     return redirect(url_for('main.profile',uname=uname))     
+
+@main.route('/review/<int:id>')
+def single_review(id):
+    review=Review.query.get(id)
+    if review is None:
+        abort(404)
+    format_review = markdown2.markdown(review.movie_review,extras=["code-friendly", "fenced-code-blocks"])
+    return render_template('review.html',review = review,format_review=format_review)    
