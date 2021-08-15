9e0e133e962e8d0bb2daec8bdc5b711952c9258e
samwel-chege <samwelchegeh09@gmail.com>
2021-08-15 14:17:10 +0300

    create TestConfig class

--- config.py
+++ config.py
@@ -31,19 +31,16 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     pass
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sammie:samm@localhost/watchlist_test'
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sammie:samm@localhost/watchlist'
     DEBUG = True
 
 config_options = {
     'development': DevConfig,
-    'production': ProdConfig
+    'production': ProdConfig,
+    'test':TestConfig
 }    
