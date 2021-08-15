70c54a26c1edf357b0730ef7464f79db6d544cc3
samwel-chege <samwelchegeh09@gmail.com>
2021-08-15 17:36:21 +0300

    instantiate simplemde in the application factory function

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -13,6 +14,7 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
 mail = Mail()
+simple = SimpleMDE
 
 
 def create_app(config_name):
@@ -32,6 +34,7 @@ def create_app(config_name):
  login_manager.init_app(app)
  db.init_app(app)
  mail.init_app(app)
+ simple.init_app(app)
 
  #Registering the blueprint
  from .main import main as main_blueprint
