7a949735555473ce4c0405e131af483b9e2ee7cd
samwel-chege <samwelchegeh09@gmail.com>
2021-08-15 10:45:16 +0300

    update save review instance defination

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,7 @@ from . import main
 from ..request import get_movies,get_movie,search_movie
 from .forms import ReviewForm,UpdateProfile
 from ..model import Review,User
-from flask_login import login_required
+from flask_login import login_required,current_user
 from .. import db,photos
 
 
@@ -60,7 +60,11 @@ def new_review(id):
     if form.validate_on_submit():
         title = form.title.data 
         review = form.review.data
-        new_review = Review(movie.id,title,movie.poster,review)
+
+        #updated review instance
+        new_review = Review(movie_id = movie.id,movie_title = title,image_path = movie.poster,movie_review = review,user= current_user)
+
+        #save review method
         new_review.save_review()
         return redirect(url_for('.movie',id = movie.id))
 
